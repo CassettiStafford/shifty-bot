c8911684148d724a978b6045ddebc567779fd8bc
CassettiStafford <40670893+CassettiStafford@users.noreply.github.com>
2019-05-18 10:42:57 -0700

    Create TestCommand.js

---
+++ dist/Commands/TestCommand.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class testCommand {
+    constructor() {
+        this._command = "testCommand";
+    }
+    help() {
+        return "This command don't do jack";
+    }
+    isThisCommand(command) {
+        return command === this._command;
+    }
+    runCommand(args, msgObject, client) {
+        msgObject.channel.send("ye fool");
+    }
+}
+exports.default = testCommand;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiVGVzdENvbW1hbmQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvQ29tbWFuZHMvVGVzdENvbW1hbmQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFHQSxNQUFxQixXQUFXO0lBQWhDO1FBRXFCLGFBQVEsR0FBRyxhQUFhLENBQUE7SUFpQjdDLENBQUM7SUFmRyxJQUFJO1FBQ0EsT0FBTyw0QkFBNEIsQ0FBQztJQUN4QyxDQUFDO0lBRUQsYUFBYSxDQUFDLE9BQWU7UUFDekIsT0FBTyxPQUFPLEtBQUssSUFBSSxDQUFDLFFBQVEsQ0FBQztJQUNyQyxDQUFDO0lBRUQsVUFBVSxDQUFDLElBQWMsRUFBRSxTQUEwQixFQUFFLE1BQXNCO1FBR3pFLFNBQVMsQ0FBQyxPQUFPLENBQUMsSUFBSSxDQUFDLFNBQVMsQ0FBQyxDQUFDO0lBQ3RDLENBQUM7Q0FHSjtBQW5CRCw4QkFtQkMifQ==
